9ca5d6c0a64212b399aae2cada8a9d1480112694
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-04 22:29:54 +0800

    Class

---
+++ Days.03.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.print("mari");
+        System.out.println(" kita");
+        System.out.println("makan");
+        System.out.println("bersama-sama ");
+        
+        
+    }
+    
+}
