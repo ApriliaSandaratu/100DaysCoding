b19c92cb5cefe71e13e7a274a39b0ef5ef231fb9
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-05 20:51:10 +0800

    Class

---
+++ Days.04.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.printf(" dia berulang tahun kemarin. %s%n " ,"april");
+        System.out.printf(" yang ke %d %n " ,18 );
+        System.out.printf(" hadiah nya sebesar %f%n" ,1.2);
+    }
+}
+    
