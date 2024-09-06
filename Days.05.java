bb7ee797d97ac7490da442d4ba34a0f9ba3936b7
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-06 23:24:22 +0800

    Class

---
+++ Days.05.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        /* komentar pertama ini akan berakhir ketika saya memberikan
+         * tanda di bawah ini
+         */
+          
+        // Ini komentar kedua menggunakan tanda garing miring berulang-ulang 
+        // seperti ini
+    }
+    
+}
