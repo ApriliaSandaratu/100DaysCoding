83ca7ea04fca8f1e6a5fb038be0ab43cb8de7e9b
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-08 00:10:05 +0800

    Class

---
+++ Days.06.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama =("Aprilia ");
+        int umur = 19;
+        double tinggi = 153.0;
+        System.out.println(nama);
+        System.out.println(umur);
+        System.out.println(tinggi);
+    }
+    
+  }
