e83bc41d702a72a7e3746433ac3417c6a1e8a19c
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-09 20:42:36 +0800

    Class

---
+++ Days.08.java
@@ -0,0 +1,58 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //sebelum di update
+        int nomer = 1;
+        System.out.println("angka : " + nomer);
+    //sesudah di update
+        nomer = 2;
+        System.out.println("angka : " + nomer);
+        
+        byte angka = 3;
+        System.out.println("nomor : " + angka);
+        angka = 4;
+        System.out.println("nomor : " + angka);
+        
+        String nama2 = "Aprilia Sandaratu";
+        System.out.println("Nama : " + nama2);
+        nama2 = "april";
+        System.out.println("Nama : " + nama2);
+        
+        long hitung = 5;
+        System.out.println("HITUNG : " + hitung);
+        hitung = 6;
+        System.out.println("HITUNG : " + hitung);
+        
+        float bilangan2 = (float) 1.20;
+        System.out.println("kg : " + bilangan2);
+        bilangan2 = (float) 2.40;
+        System.out.println("kg : " + bilangan2);
+        
+        double panjang2 = 1.20;
+        System.out.println("tinggi : " + panjang2 + 'M');
+        panjang2 = 1.80;
+        System.out.println("tinggi : " + panjang2 + 'M');
+        
+        short tahun = 1990;
+        System.out.println("Tahun : " + tahun);
+        tahun = 1998;
+        System.out.println("Tahun : " + tahun);
+        
+        char variabel = 'A';
+        System.out.println("kapital : " + variabel);
+        variabel = 'P';
+        System.out.println("kapital : " + variabel);
+        
+        boolean sakit = true;
+        System.out.println("kuliah : " + sakit);
+        sakit = false;
+        System.out.println("hujan  : " + sakit);
+        
+        
+    }
+    
+
+    }
+
