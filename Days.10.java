2a3f6625ca13d2ac31f2d5bf50b74fba143f0590
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-11 11:30:02 +0800

    Class

---
+++ Days.10.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+      byte PanjangTali = 14;
+       System.out.println( "PanjangTali = " + PanjangTali);      
+    }
+    
+}
