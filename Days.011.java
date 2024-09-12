1c1e76dcd2342b6315394a8261769feb0670a232
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-12 21:49:16 +0800

    Class

---
+++ Days.011.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tipe data short
+        short kapasitas = 150;
+        System.out.println( " kapasitas= " + kapasitas);
+    }
+    
+}
