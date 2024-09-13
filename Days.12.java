43bb53f2d17ce0ad2f4fe64f908ef80263df27de
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-13 21:45:41 +0800

    Class

---
+++ Days.12.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tie data integer
+        int angka = 20;
+        System.out.print(angka);
+    }
+    
+}
