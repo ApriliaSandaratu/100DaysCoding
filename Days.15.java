d996845ce34937c412a9b6d23a23800f75f05eef
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-16 23:43:01 +0800

    Class

---
+++ Days.15.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tipe data double
+        double aa = 12;
+        System.out.println(aa);
+    }
+    
+}
