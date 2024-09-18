f08eeb92b005816e4772f476958a638ea38353bc
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-18 21:37:14 +0800

    Class

---
+++ Days.17.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tipe data char
+        char angka = '4';
+        System.out.println(" angka = " + angka );
+    }
+    
+}
