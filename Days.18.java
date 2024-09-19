c5740bc1e2174f1cc9a6eea0b13e87c9ebc2dd7f
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-19 19:18:28 +0800

    Class

---
+++ Days.18.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tipe data boolean
+        boolean INF = true;
+        System.out.println("INF = " + INF );
+    }
+    
+}
