35a3a7ac3735aa2d36be926dd8545fd95a7a9681
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-22 23:41:01 +0800

    Class

---
+++ Days.21.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tipe data short
+        Short umur = 10;
+        System.out.println( umur );
+    }
+    
+}
