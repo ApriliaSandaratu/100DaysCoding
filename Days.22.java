5d0bff6dad90cc09e422a9aff4d9889b58e6c69c
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-23 22:21:43 +0800

    Class

---
+++ Days.22.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tipe data integer
+        Integer tinggi = 15;
+        System.out.println("tinggi = " + tinggi);
+    }
+    
+}
