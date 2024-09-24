b10f48263d5529b2255c8d618e319e81b70dc752
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-24 23:47:46 +0800

    Class

---
+++ Days.23.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tipe data long
+        Long a = 50l;
+        System.out.println(a);
+    }
+    
+}
