d6279d40a9ee4f752d7e5454262785d09f2f9ad1
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-25 23:28:19 +0800

    Class

---
+++ Days.24.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Float tinggi = 150.3f;
+        System.out.println("tinggi = " + tinggi);
+    }
+    
+}
