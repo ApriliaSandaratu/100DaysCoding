318a14f2eb061eaab4d7dc54d20b5a3f1f9104a6
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-26 20:44:23 +0800

    Class

---
+++ Days.25.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Double tinggi = 25.5;
+        System.out.println("tinggi air = " + tinggi);
+    }
+    
+}
