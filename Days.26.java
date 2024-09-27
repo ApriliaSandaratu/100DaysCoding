6a8f6fa5eba87a405767913a3e2764c2b0430328
ApriliaSandaratu <rudytabootie32@gmail.com>
2024-09-27 22:46:00 +0800

    Class

---
+++ Days.26.java
@@ -0,0 +1,11 @@
+package Java;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //contoh tipe data reference character
+        Character kelas = 'A';
+        System.out.println("kelas = " + kelas );
+    }
+    
+}
